04175fdd1209d52f2be8ecf309c654202f76faf6
Aurel1359 <157637838+Aurel1359@users.noreply.github.com>
2025-04-15 12:38:29 +0700

    Add files via upload

--- index.blade.php
+++ index.blade.php
@@ -1,30 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Absensi Siswa</title>
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
 </head>
-
 <body>
-    @include('layouts.headadmin')
+    @include('layouts.headguest')
     <div class="container mt-4">
         <p class="col-md-12 mb-0 text-muted">
-            <h3 class="mb-4">Selamat datang di Halaman
+            <h3 class="mb-4"> Selamat datang di Halaman
                 <font color="blue">
                     {{ Auth::user()->admin_type }}
                 </font>
             </h3> <br>
-            Silahkan Anda melakukan proses untuk Absensi Siswa yang diperlukan untuk data kedisiplinan siswa
+            Silahkan Anda klik menu Absensi untuk melihat data kedisiplinan Siswa
         </p>
-        <!--<div class="mt-5 p-4 bg-white shadow-sm rounded">
-            <h5>💡 Info:</h5>
-            <p>Panel ini memudahkan siswa siswi mengakses data terlambat.</p>
-        </div>-->
-    </div>
+            <!--<div class="mt-5 p-4 bg-white shadow-sm rounded">
+                <h5>💡 Info:</h5>
+                <p>Panel ini memudahkan siswa siswi mengakses data terlambat.</p>
+            </div>-->
+        </div>
     @include('layouts.footer')
 </body>
 </html>
@@ -33,29 +31,27 @@
 <!-- Amalia -->
 <!-- <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Absensi Siswa</title>
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
 </head>
-
 <body>
-    @include('layouts.headadmin')
+    @include('layouts.headguest')
     <div class="container mt-4">
         <p class="col-md-12 mb-0 text-muted">
-        <h2 class="mb-4">Selamat datang di Halaman
-            <font color="blue">
-                {{ Auth::user()->admin_type }}
-            </font>
-        </h2>
-        <div class="mt-5 p-4 bg-white shadow-sm rounded">
-            <h5>💡 Info:</h5>
-            <p>Panel ini memudahkan siswa siswi mengakses data terlambat.</p>
+            <h2 class="mb-4">Selamat datang di Halaman
+                <font color="blue">
+                    {{ Auth::user()->admin_type }}
+                </font>
+            </h2>
+            <div class="mt-5 p-4 bg-white shadow-sm rounded">
+                <h5>💡 Info:</h5>
+                <p>Panel ini memudahkan siswa siswi mengakses data terlambat.</p>
+            </div>
         </div>
-    </div>
     @include('layouts.footer')
 </body>
 </html> -->
